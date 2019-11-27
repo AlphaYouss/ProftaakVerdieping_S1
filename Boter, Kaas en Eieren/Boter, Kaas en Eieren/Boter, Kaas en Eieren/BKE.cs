0f833b836600b2df849499013bf3c3dceadbbac7
Sem Kivits <Semkivits@gmail.com>
2019-11-27 09:13:27 +0100

    Boter, kaas en eieren v1

---
+++ Boter, Kaas en Eieren/Boter, Kaas en Eieren/Boter, Kaas en Eieren/BKE.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Boter__Kaas_en_Eieren
+{
+    class BKE
+    {
+       public List<string> ImageBoxen = new List<string>() {"vak0", "vak1", "vak2", "vak3", "vak4", "vak5", "vak6", "vak7", "vak8", "vak9" }; //Vakken van het bord.
+                                                                                                                                              // "0" is geen vak, maar maakt het aanroepen van elementen overzichtelijker.
+
+        public int aiClick = 0;
+        public BKE()
+        {
+           
+        }
+
+        public bool Checken() // Checken of iemand heeft gewonnen
+        {
+            if  (  //horizontaal  
+                   ImageBoxen[1] == ImageBoxen[2] && ImageBoxen[2] == ImageBoxen[3] 
+                || ImageBoxen[4] == ImageBoxen[5] && ImageBoxen[5] == ImageBoxen[6]
+                || ImageBoxen[7] == ImageBoxen[8] && ImageBoxen[8] == ImageBoxen[9]
+
+                   //verticaal
+                || ImageBoxen[1] == ImageBoxen[4] && ImageBoxen[4] == ImageBoxen[7] 
+                || ImageBoxen[2] == ImageBoxen[5] && ImageBoxen[5] == ImageBoxen[8]
+                || ImageBoxen[3] == ImageBoxen[6] && ImageBoxen[6] == ImageBoxen[9]
+
+                   //diagonaal
+                || ImageBoxen[1] == ImageBoxen[5] && ImageBoxen[5] == ImageBoxen[9] 
+                || ImageBoxen[3] == ImageBoxen[5] && ImageBoxen[5] == ImageBoxen[7]
+                )
+            {
+                return true;
+            }
+            return false;
+        }
+        
+        public void PlayerClick(int vak)
+        {
+            ImageBoxen[vak] = "X";
+        }
+        
+        public int GenereerNummer()
+        {
+            Random rnd = new Random();
+            int vak = rnd.Next(1, 10);
+            return vak;
+        }
+
+        public void AIclick()
+        {
+            bool check = true;
+
+            do
+            {
+                int getal = GenereerNummer();
+
+                if (ImageBoxen[getal] == "X" || ImageBoxen[getal] == "O")
+                {
+
+                }
+                else
+                {
+                    ImageBoxen[getal] = "O";
+                    aiClick = getal;
+
+                    check = false;
+                }
+
+            } while (check == true);
+        }
+    }
+}
--- Boter, Kaas en Eieren/Boter, Kaas en Eieren/Boter, Kaas en Eieren/MainPage.xaml.cs
+++ Boter, Kaas en Eieren/Boter, Kaas en Eieren/Boter, Kaas en Eieren/MainPage.xaml.cs
@@ -11,7 +11,9 @@ using Windows.UI.Xaml.Controls.Primitives;
 using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Media.Imaging;
 using Windows.UI.Xaml.Navigation;
+
 
 // The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
 
@@ -22,9 +24,121 @@ namespace Boter__Kaas_en_Eieren
     /// </summary>
     public sealed partial class MainPage : Page
     {
+        BKE Speler = new BKE();
+      //  BKE AI = new BKE();
+
         public MainPage()
         {
             this.InitializeComponent();
+        }
+
+        private void ae(object sender, RoutedEventArgs e)
+        {
+            string ctrlName = ((Control)sender).Name;
+            string[] words = ctrlName.Split("vak");
+
+            Speler.PlayerClick(Convert.ToInt32(words[1]));
+
+            ChangeImage(Convert.ToInt32(words[1]));
+
+            if (Speler.Checken())
+            {
+                string a = "gewonnen!";
+                txtMessage.Text = a;
+            }
+
+            Speler.AIclick();
+            ChangeImage2(Speler.aiClick);
+        }
+
+        private void ChangeImage(int nummer)
+        {
+            switch (nummer)
+            {
+                default:
+                    break;
+                case 1:
+                    image.Source = new BitmapImage(new Uri("ms-appx:///Assets/Dikke x.jpg"));
+                    vak1.IsEnabled = false;
+                    break;
+                case 2:
+                    image2.Source = new BitmapImage(new Uri("ms-appx:///Assets/Dikke x.jpg"));
+                    vak2.IsEnabled = false;
+                    break;
+                case 3:
+                    image3.Source = new BitmapImage(new Uri("ms-appx:///Assets/Dikke x.jpg"));
+                    vak3.IsEnabled = false;
+                    break;
+                case 4:
+                    image4.Source = new BitmapImage(new Uri("ms-appx:///Assets/Dikke x.jpg"));
+                    vak4.IsEnabled = false;
+                    break;
+                case 5:
+                    image5.Source = new BitmapImage(new Uri("ms-appx:///Assets/Dikke x.jpg"));
+                    vak5.IsEnabled = false;
+                    break;
+                case 6:
+                    image6.Source = new BitmapImage(new Uri("ms-appx:///Assets/Dikke x.jpg"));
+                    vak6.IsEnabled = false;
+                    break;
+                case 7:
+                    image7.Source = new BitmapImage(new Uri("ms-appx:///Assets/Dikke x.jpg"));
+                    vak7.IsEnabled = false;
+                    break;
+                case 8:
+                    image8.Source = new BitmapImage(new Uri("ms-appx:///Assets/Dikke x.jpg"));
+                    vak8.IsEnabled = false;
+                    break;
+                case 9:
+                    image9.Source = new BitmapImage(new Uri("ms-appx:///Assets/Dikke x.jpg"));
+                    vak9.IsEnabled = false;
+                    break;
+
+            }
+        }
+        private void ChangeImage2(int nummer)
+        {
+            switch (nummer)
+            {
+                default:
+                    break;
+                case 1:
+                    image.Source = new BitmapImage(new Uri("ms-appx:///Assets/Dikke O.jpg"));
+                    vak1.IsEnabled = false;
+                    break;
+                case 2:
+                    image2.Source = new BitmapImage(new Uri("ms-appx:///Assets/Dikke O.jpg"));
+                    vak2.IsEnabled = false;
+                    break;
+                case 3:
+                    image3.Source = new BitmapImage(new Uri("ms-appx:///Assets/Dikke O.jpg"));
+                    vak3.IsEnabled = false;
+                    break;
+                case 4:
+                    image4.Source = new BitmapImage(new Uri("ms-appx:///Assets/Dikke O.jpg"));
+                    vak4.IsEnabled = false;
+                    break;
+                case 5:
+                    image5.Source = new BitmapImage(new Uri("ms-appx:///Assets/Dikke O.jpg"));
+                    vak5.IsEnabled = false;
+                    break;
+                case 6:
+                    image6.Source = new BitmapImage(new Uri("ms-appx:///Assets/Dikke O.jpg"));
+                    vak6.IsEnabled = false;
+                    break;
+                case 7:
+                    image7.Source = new BitmapImage(new Uri("ms-appx:///Assets/Dikke O.jpg"));
+                    vak7.IsEnabled = false;
+                    break;
+                case 8:
+                    image8.Source = new BitmapImage(new Uri("ms-appx:///Assets/Dikke O.jpg"));
+                    vak8.IsEnabled = false;
+                    break;
+                case 9:
+                    image9.Source = new BitmapImage(new Uri("ms-appx:///Assets/Dikke O.jpg"));
+                    vak9.IsEnabled = false;
+                    break;
+            }
         }
     }
 }
