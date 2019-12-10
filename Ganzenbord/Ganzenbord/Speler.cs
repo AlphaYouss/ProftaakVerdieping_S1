5d318a3e6f239059822bd9bcf9f166b415cc0e61
Sem Kivits <Semkivits@gmail.com>
2019-12-10 14:19:17 +0100

    Ganzenbord speler

---
+++ Ganzenbord/Ganzenbord/Speler.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI.Xaml.Media.Imaging;
+
+namespace Ganzenbord
+{
+    class Speler
+    {
+        public Ganzenbord ganzenbord { get; private set; }
+        public Bord bord { get; private set; }
+        public bool beurt { get; private set; }
+        public string naamSpeler { get; private set; }
+        public BitmapImage spelerPlaatje { get; private set; }
+        public int locatie { get; private set; }
+
+        public Speler(Bord bord)
+        {
+            ganzenbord = new Ganzenbord();
+            this.bord = bord;
+            locatie = 0;
+        }
+
+        private void ZetStap(int locatie)
+        {
+
+            locatie = locatie + ganzenbord.Dobbellen();
+        }
+    }
+}
